ec4573f510bacce6a65e687e5d9a29f232fe1ace
Robert Marki <gsmiko@gmail.com>
2018-05-27 09:13:21 +0200

    Implement Application class

---
+++ rabbit_force/app.py
@@ -0,0 +1,184 @@
+"""Application class definition"""
+import asyncio
+import logging
+
+import uvloop
+
+from .factories import create_message_sink, create_message_source, \
+    create_router
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Application:  # pylint: disable=too-few-public-methods
+    """Rabbit force application"""
+
+    def __init__(self, config):
+        """
+        The application configures itself the first time :meth:`run` is called.
+        If you want to run the application with a different configuration then
+        a new Application instance should be created.
+
+        :param dict config: Application configuration
+        """
+        #: The application's configuration
+        self.config = config
+        #: Marks whether the application is already configured or not
+        self._configured = False
+        #: A message source object
+        self._source = None
+        #: A message sink object
+        self._sink = None
+        #: A message router object
+        self._router = None
+        #: The set of currently running message forwarding tasks
+        self._forwarding_tasks = set()
+        #: Event loop
+        self._loop = None
+
+    def run(self):
+        """Run the Rabbit force application, listen for and forward messages
+        until a keyboard interrupt occurs"""
+
+        # use the uvloop event loop policy
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+        # create an event loop and create the main task
+        self._loop = asyncio.get_event_loop()
+        task = asyncio.ensure_future(self._run(), loop=self._loop)
+
+        # run the task until completion
+        try:
+            self._loop.run_until_complete(task)
+
+        # on a keyboard interrupt cancel the main task and await its completion
+        except KeyboardInterrupt:
+            task.cancel()
+            self._loop.run_until_complete(task)
+
+    async def _run(self):
+        """Configure the application and listen for incoming messages until
+        cancellation"""
+
+        # configure the application
+        await self._configure()
+
+        # listen for incoming messages
+        await self._listen_for_messages()
+
+    async def _configure(self):
+        """Create and configure collaborator objects"""
+
+        # if the application is already configured skip the configuration step
+        if not self._configured:
+            self._source = await create_message_source(
+                **self.config["source"]
+            )
+            self._sink = await create_message_sink(**self.config["sink"])
+            self._router = create_router(**self.config["router"])
+            self._configured = True
+
+    async def _listen_for_messages(self):
+        """Listen for incoming messages and route them to the appropriate
+        brokers
+
+        This method will block until it's cancelled. On cancellation it'll
+        drain all the pending messages and forwarding tasks.
+        """
+        # open the message source
+        await self._source.open()
+
+        # consume messages until the message source is not closed, or until
+        # all the messages are consumed from a closed message source
+        while not self._source.closed or self._source.has_pending_messages:
+            try:
+                # await an incoming message
+                source_name, message = await self._source.get_message()
+
+                # forward the message in non blocking fashion (without awaiting
+                # the tasks result)
+                await self._schedule_message_forwarding(source_name, message)
+
+            # on cancellation close the message source but continue to
+            # consume pending messages until there is no more left
+            except asyncio.CancelledError:
+                await self._source.close()
+
+        # if the source is closed and there are no more messages to consume,
+        # await the completion of scheduled forwaring tasks
+        await self._wait_scheduled_forwarding_tasks()
+
+        # when all the messages are forwarded close the message sink
+        await self._sink.close()
+
+    async def _schedule_message_forwarding(self, source_name, message):
+        """Create a task for forwarding the *message* from *source_name* and
+        add it to the list of active forwarding tasks
+
+        :param str source_name: Name of the message source
+        :param dict message: A message
+        """
+        # create a task to forward the message
+        forwarding_task = asyncio.ensure_future(
+            self._forward_message(source_name, message),
+            loop=self._loop
+        )
+        # set a callback to consume the tasks result
+        forwarding_task.add_done_callback(self._forward_message_done)
+        # add the task to the set of running tasks
+        self._forwarding_tasks.add(forwarding_task)
+
+    async def _wait_scheduled_forwarding_tasks(self):
+        """Wait for all the active forwarding tasks to complete"""
+
+        # check if there are any running forwarding tasks, and await them
+        if self._forwarding_tasks:
+            await asyncio.wait(self._forwarding_tasks, loop=self._loop)
+
+    async def _forward_message(self, source_name, message):
+        """Forward the *message* from *source_name* with the appropriate route
+
+        :param str source_name: Name of the message source
+        :param dict message: A message
+        :return: A three element tuple which contains the forwarded message, \
+        name of the message source and the routing parameters
+        :rtype: tuple[dict, str, Route]
+        """
+        # find a matching route for the message
+        route = self._router.find_route(source_name, message)
+
+        # if a route was found for the message then forward it using the
+        # routing parameters
+        if route is not None:
+            await self._sink.consume_message(message,
+                                             route.broker_name,
+                                             route.exchange_name,
+                                             route.routing_key,
+                                             route.properties)
+
+        # return the message, source_name and the routing parameters
+        return message, source_name, route
+
+    def _forward_message_done(self, future):
+        """Consume the result of a completed message forwarding task
+
+        :param asyncio.Future future: A future object
+        """
+        self._forwarding_tasks.remove(future)
+
+        try:
+            message, source_name, route = future.result()
+            channel = message["channel"]
+            replay_id = message["data"]["event"]["replayId"]
+
+            if route:
+                LOGGER.info("Message %r on channel %r "
+                            "from %r forwarded to %r.",
+                            replay_id, channel, source_name, route)
+            else:
+                LOGGER.warning("No route found for message "
+                               "%r on channel %r from %r, message dropped.",
+                               replay_id, channel, source_name)
+        except Exception:  # pylint: disable=broad-except
+            LOGGER.exception("Failed to forward message.")
---
+++ tests/test_app.py
@@ -0,0 +1,291 @@
+import asyncio
+
+from asynctest import TestCase, mock
+
+from rabbit_force.app import Application
+from rabbit_force.routing import Route
+
+
+class TestApplication(TestCase):
+    def setUp(self):
+        self.config = object()
+        self.app = Application(self.config)
+
+    def test_init(self):
+        self.assertIs(self.app.config, self.config)
+        self.assertIsNone(self.app._source)
+        self.assertIsNone(self.app._sink)
+        self.assertIsNone(self.app._router)
+        self.assertFalse(self.app._configured)
+        self.assertEqual(self.app._forwarding_tasks, set())
+        self.assertIsNone(self.app._loop)
+
+    async def test__run(self):
+        self.app._configure = mock.CoroutineMock()
+        self.app._listen_for_messages = mock.CoroutineMock()
+
+        await self.app._run()
+
+        self.app._configure.assert_called()
+        self.app._listen_for_messages.assert_called()
+
+    @mock.patch("rabbit_force.app.create_message_source")
+    @mock.patch("rabbit_force.app.create_message_sink")
+    @mock.patch("rabbit_force.app.create_router")
+    async def test_configure(self, create_router, create_message_sink,
+                             create_message_source):
+        self.app.config = {
+            "source": {"key1": "value1"},
+            "sink": {"key2": "value2"},
+            "router": {"key3": "value3"}
+        }
+
+        await self.app._configure()
+
+        create_message_source.assert_called_with(**self.app.config["source"])
+        create_message_sink.assert_called_with(**self.app.config["sink"])
+        create_router.assert_called_with(**self.app.config["router"])
+        self.assertTrue(self.app._configured)
+
+    @mock.patch("rabbit_force.app.create_message_source")
+    @mock.patch("rabbit_force.app.create_message_sink")
+    @mock.patch("rabbit_force.app.create_router")
+    async def test_configure_if_already_configured(self, create_router,
+                                                   create_message_sink,
+                                                   create_message_source):
+        self.app.config = {
+            "source": {"key1": "value1"},
+            "sink": {"key2": "value2"},
+            "router": {"key3": "value3"}
+        }
+        self.app._configured = True
+
+        await self.app._configure()
+
+        create_message_source.assert_not_called()
+        create_message_sink.assert_not_called()
+        create_router.assert_not_called()
+        self.assertTrue(self.app._configured)
+
+    @mock.patch("rabbit_force.app.asyncio")
+    async def test_schedule_message_forwarding(self, asyncio_mod):
+        self.app._loop = self.loop
+        coro = object()
+        self.app._forward_message = mock.MagicMock(return_value=coro)
+        task = mock.MagicMock()
+        asyncio_mod.ensure_future.return_value = task
+        source_name = "source"
+        message = object()
+
+        await self.app._schedule_message_forwarding(source_name, message)
+
+        self.app._forward_message.assert_called_with(source_name, message)
+        asyncio_mod.ensure_future.assert_called_with(coro, loop=self.loop)
+        task.add_done_callback.assert_called_with(
+            self.app._forward_message_done
+        )
+        self.assertEqual(self.app._forwarding_tasks, {task})
+
+    @mock.patch("rabbit_force.app.asyncio")
+    async def test_wait_scheduled_forwarding_tasks(self, asyncio_mod):
+        self.app._loop = self.loop
+        self.app._forwarding_tasks = {object()}
+        asyncio_mod.wait = mock.CoroutineMock()
+
+        await self.app._wait_scheduled_forwarding_tasks()
+
+        asyncio_mod.wait.assert_called_with(self.app._forwarding_tasks,
+                                            loop=self.loop)
+
+    @mock.patch("rabbit_force.app.asyncio")
+    async def test_wait_scheduled_forwarding_tasks_without_tasks(self,
+                                                                 asyncio_mod):
+        self.app._loop = self.loop
+        self.app._forwarding_tasks = set()
+        asyncio_mod.wait = mock.CoroutineMock()
+
+        await self.app._wait_scheduled_forwarding_tasks()
+
+        asyncio_mod.wait.assert_not_called()
+
+    async def test_forward_message(self):
+        self.app._router = mock.MagicMock()
+        route = Route(broker_name="broker", exchange_name="exchange",
+                      routing_key="key", properties={})
+        self.app._router.find_route.return_value = route
+        self.app._sink = mock.MagicMock()
+        self.app._sink.consume_message = mock.CoroutineMock()
+        source_name = "source"
+        message = object()
+
+        result = await self.app._forward_message(source_name, message)
+
+        self.assertEqual(result[0], message)
+        self.assertEqual(result[1], source_name)
+        self.assertEqual(result[2], route)
+        self.app._router.find_route.assert_called_with(source_name, message)
+        self.app._sink.consume_message.assert_called_with(
+            message, route.broker_name, route.exchange_name, route.routing_key,
+            route.properties
+        )
+
+    async def test_forward_message_without_route(self):
+        self.app._router = mock.MagicMock()
+        route = None
+        self.app._router.find_route.return_value = route
+        self.app._sink = mock.MagicMock()
+        self.app._sink.consume_message = mock.CoroutineMock()
+        source_name = "source"
+        message = object()
+
+        result = await self.app._forward_message(source_name, message)
+
+        self.assertEqual(result[0], message)
+        self.assertEqual(result[1], source_name)
+        self.assertIsNone(result[2])
+        self.app._router.find_route.assert_called_with(source_name, message)
+        self.app._sink.consume_message.assert_not_called()
+
+    def test_forward_message_done(self):
+        future = mock.MagicMock()
+        replay_id = 12
+        channel = "channel"
+        message = {
+            "channel": channel,
+            "data": {"event": {"replayId": replay_id}}
+        }
+        source_name = "source"
+        route = object()
+        future.result.return_value = (message, source_name, route)
+        self.app._forwarding_tasks = {future}
+
+        with self.assertLogs("rabbit_force.app", "DEBUG") as log:
+            self.app._forward_message_done(future)
+
+        self.assertEqual(log.output, [
+            f"INFO:rabbit_force.app:Message {replay_id!r} on channel "
+            f"{channel!r} from {source_name!r} forwarded to {route!r}."
+        ])
+        self.assertFalse(self.app._forwarding_tasks)
+
+    def test_forward_message_done_without_route(self):
+        future = mock.MagicMock()
+        replay_id = 12
+        channel = "channel"
+        message = {
+            "channel": channel,
+            "data": {"event": {"replayId": replay_id}}
+        }
+        source_name = "source"
+        route = None
+        future.result.return_value = (message, source_name, route)
+        self.app._forwarding_tasks = {future}
+
+        with self.assertLogs("rabbit_force.app", "DEBUG") as log:
+            self.app._forward_message_done(future)
+
+        self.assertEqual(log.output, [
+            f"WARNING:rabbit_force.app:No route found for message "
+            f"{replay_id!r} on channel {channel!r} from {source_name!r}, "
+            f"message dropped."
+        ])
+        self.assertFalse(self.app._forwarding_tasks)
+
+    def test_forward_message_done_on_error(self):
+        future = mock.MagicMock()
+        future.result.side_effect = TypeError()
+        self.app._forwarding_tasks = {future}
+
+        with self.assertLogs("rabbit_force.app", "DEBUG") as log:
+            self.app._forward_message_done(future)
+
+        self.assertTrue(log.output[0].startswith(
+            "ERROR:rabbit_force.app:Failed to forward message."
+        ))
+        self.assertFalse(self.app._forwarding_tasks)
+
+    @mock.patch("rabbit_force.app.uvloop")
+    @mock.patch("rabbit_force.app.asyncio")
+    async def test_run(self, asyncio_mod, uvloop_mod):
+        task = mock.MagicMock()
+        asyncio_mod.ensure_future.return_value = task
+        self.app._run = mock.MagicMock()
+        loop = mock.MagicMock()
+        loop.run_until_complete.side_effect = (KeyboardInterrupt, None)
+        asyncio_mod.get_event_loop.return_value = loop
+
+        self.app.run()
+
+        asyncio_mod.set_event_loop_policy.assert_called_with(
+            uvloop_mod.EventLoopPolicy.return_value
+        )
+        self.assertEqual(self.app._loop, loop)
+        asyncio_mod.ensure_future.assert_called_with(
+            self.app._run.return_value, loop=loop
+        )
+        task.cancel.assert_called()
+        loop.run_until_complete.assert_has_calls([mock.call(task)] * 2)
+
+    async def test_listen_for_messages(self):
+        source = mock.MagicMock()
+        closed = mock.PropertyMock(side_effect=(False, True, True))
+        type(source).closed = closed
+        has_pending_messages = mock.PropertyMock(side_effect=(True, False))
+        type(source).has_pending_messages = has_pending_messages
+        source.close = mock.CoroutineMock()
+        source.open = mock.CoroutineMock()
+        self.app._source = source
+        message1 = object()
+        message2 = object()
+        source1 = object()
+        source2 = object()
+        source.get_message = mock.CoroutineMock(
+            side_effect=((source1, message1), (source2, message2))
+        )
+        self.app._schedule_message_forwarding = mock.CoroutineMock()
+        self.app._wait_scheduled_forwarding_tasks = mock.CoroutineMock()
+        self.app._sink = mock.MagicMock()
+        self.app._sink.close = mock.CoroutineMock()
+
+        await self.app._listen_for_messages()
+
+        source.open.assert_called()
+        self.assertEqual(self.app._schedule_message_forwarding.mock_calls, [
+            mock.call(source1, message1),
+            mock.call(source2, message2)
+        ])
+        self.app._wait_scheduled_forwarding_tasks.assert_called()
+        self.app._sink.close.assert_called()
+
+    async def test_listen_for_messages_cancelled(self):
+        source = mock.MagicMock()
+        closed = mock.PropertyMock(side_effect=(False, False, True, True))
+        type(source).closed = closed
+        has_pending_messages = mock.PropertyMock(side_effect=(True, False))
+        type(source).has_pending_messages = has_pending_messages
+        source.close = mock.CoroutineMock()
+        source.open = mock.CoroutineMock()
+        self.app._source = source
+        message1 = object()
+        message2 = object()
+        source1 = object()
+        source2 = object()
+        source.get_message = mock.CoroutineMock(
+            side_effect=((source1, message1), asyncio.CancelledError(),
+                         (source2, message2))
+        )
+        self.app._schedule_message_forwarding = mock.CoroutineMock()
+        self.app._wait_scheduled_forwarding_tasks = mock.CoroutineMock()
+        self.app._sink = mock.MagicMock()
+        self.app._sink.close = mock.CoroutineMock()
+
+        await self.app._listen_for_messages()
+
+        source.open.assert_called()
+        self.assertEqual(self.app._schedule_message_forwarding.mock_calls, [
+            mock.call(source1, message1),
+            mock.call(source2, message2)
+        ])
+        source.close.assert_called()
+        self.app._wait_scheduled_forwarding_tasks.assert_called()
+        self.app._sink.close.assert_called()
