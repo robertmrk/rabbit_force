3786ae81608948cf9e5d1b3db89064520d6b5a49
Robert Marki <mrobi@manufaktura.rs>
2018-12-06 11:08:30 +0100

    Update outdated dependencies

--- tests/test_config.py
+++ tests/test_config.py
@@ -24,7 +24,7 @@ class TestStrictSchema(TestCase):
         }
 
         with self.assertRaisesRegex(ValidationError,
-                                    r"{'bar': \['Unknown field'\]}"):
+                                    r"{'bar': \['Unknown field.'\]}"):
             self.schema_cls().load(data)
 
 
