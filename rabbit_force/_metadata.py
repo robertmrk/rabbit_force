4acccf37641c14f340625b359173c4e405aa43a6
Robert Marki <mrobi@manufaktura.rs>
2019-05-15 13:35:53 +0200

    Bump version number to 0.1.1

--- rabbit_force/_metadata.py
+++ rabbit_force/_metadata.py
@@ -4,6 +4,6 @@ DESCRIPTION = "Salesforce Streaming API to RabbitMQ adapter service"
 KEYWORDS = "salesforce streaming-api rabbitmq amqp redis asyncio " \
            "adapter service jsonpath"
 URL = "https://github.com/robertmrk/rabbit_force"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 AUTHOR = "Róbert Márki"
 AUTHOR_EMAIL = "gsmiko@gmail.com"
